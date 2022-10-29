10ca98c068d71249291d6bbbd0db705ea33f097b
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-29 20:55:51 +0530

    fixed the date to convert it to GMT+5.30

--- app/src/main/java/iot/lab/qrdetails/RecordAdapter.kt
+++ app/src/main/java/iot/lab/qrdetails/RecordAdapter.kt
@@ -1,5 +1,6 @@
 package iot.lab.qrdetails
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import iot.lab.qrdetails.model.Post
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RecordAdapter(private val context: Context) : RecyclerView.Adapter<RecordAdapter.MyViewHolder>() {
     private var data : List<Post> = emptyList()
@@ -16,15 +19,34 @@ class RecordAdapter(private val context: Context) : RecyclerView.Adapter<RecordA
         return MyViewHolder(itemView)
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
         val currentItem = data[position]
 
-        val correctedInTime = currentItem.in_time.removeRange(19 , 24).replace("T" , " \tTime : ")
-        val correctedOutTime = currentItem.out_time.removeRange(19,24).replace("T" , " \tTime : ")
+        //Format which we received From the API call
+        val formatReceived = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:SS.sss'Z'")
+
+        //Setting TimeZone to Indian which is GMT_5.30
+        formatReceived.timeZone = TimeZone.getTimeZone("GMT+5.30")
+
+        //Setting Our Desired Format of Date and Time
+        val desiredFormat = SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a")
+
+        //Making Date objects from the in time and out Time strings
+        val dateInTime = formatReceived.parse(currentItem.in_time)!!
+        val dateOutTime = formatReceived.parse(currentItem.out_time)!!
+
+        // Parsing the date Objects which returns the String in the format as specified in desiredFormat variable
+        val correctedInTime = desiredFormat.format(dateInTime)
+        val correctedOutTime = desiredFormat.format(dateOutTime)
+
+        //Setting up the data to the Views
         holder.roll.text = context.getString(R.string.roll_no , currentItem.roll)
         holder.inTime.text = context.getString(R.string.in_time , correctedInTime)
         holder.outTime.text = context.getString(R.string.out_time , correctedOutTime)
+
+
     }
 
     override fun getItemCount(): Int {
