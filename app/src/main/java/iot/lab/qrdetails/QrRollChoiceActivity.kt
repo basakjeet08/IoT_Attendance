356bceb70e5548cb5f861a848e6c22fbdf1c5897
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-31 20:35:09 +0530

    added the new Enter Roll UI for the Registration details

---
+++ app/src/main/java/iot/lab/qrdetails/QrRollChoiceActivity.kt
@@ -0,0 +1,26 @@
+package iot.lab.qrdetails
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import iot.lab.qrdetails.databinding.ActivityQrRollChoiceBinding
+
+class QrRollChoiceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQrRollChoiceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQrRollChoiceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnQRScanner.setOnClickListener {
+            val intent = Intent(this , MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnRollNumber.setOnClickListener {
+
+        }
+    }
+}
