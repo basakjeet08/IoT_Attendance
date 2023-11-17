e379822e551dd031841051779e0d4fbb4fb6d43b
Anirban Basak <jeetbasak2002@gmail.com>
2023-11-17 07:47:21 +0530

    refactor - Api interface minor refactoring

--- app/src/main/java/iot/lab/qrdetails/data/remote/SimpleApi.kt
+++ app/src/main/java/iot/lab/qrdetails/data/remote/SimpleApi.kt
@@ -1,13 +1,14 @@
 package iot.lab.qrdetails.data.remote
 
 import iot.lab.qrdetails.data.model.EventData
+import iot.lab.qrdetails.util.Constants
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface SimpleApi {
 
     // For fetching the registration details of a single roll Number
-    @GET("innovance_registration")
+    @GET(Constants.REGISTRATION_ENDPOINT)
     suspend fun getRegistrationDetails(
         @Query("filter[id][_eq]") number: String
     ): retrofit2.Response<EventData>
